75b62552e812e17ba27e2c5a152614c2e0d1fea3
OpenShift guest <andrejs.dasko@gmail.com>
2013-07-10 15:41:42 -0400

    _

--- web.js
+++ web.js
@@ -4,7 +4,9 @@ var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  var buffer = new Buffer();
+  buffer.write(fs.readFileSync("index.html"));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
